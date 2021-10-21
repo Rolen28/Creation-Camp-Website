3c5fb53f9214879ea21243ee86de24effee37894
Abdullah Khan <91890954+AbdullahKhan28@users.noreply.github.com>
2021-10-21 19:30:23 +0000

    Changed Form email receiver, Added JavaScript restrictions to forms, added drop down menus to the Contact Forms

---
+++ script.js
@@ -0,0 +1,95 @@
+function validateEmail(emailId) {
+  var mailformat = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+  if(emailId.value.match(mailformat)) {
+    alert("Thank you for submitting!");
+    document.form1.text1.focus();
+  }
+
+  else {
+    alert("Invalid email address.");
+    document.form1.text1.focus();
+    return false;
+  }
+}    
+
+function validateForm() {
+  let x = document.TitleForm.name.value;
+  let y = document.TitleForm.age.value;
+  let z = document.TitleForm.phonenumber.value;
+  let a = document.TitleForm.email.value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+
+  else {
+    if (y == "") {
+      alert("Age must be filled out");
+      return false;
+    }
+
+    else{
+      if (isNaN(y) || x < 0) {
+      alert("Age must be a numerical value greater than or equal to 0");
+      return false;
+      }
+
+      else {
+        if (z == "") {
+          alert("Phone number must be filled out");
+          return false;
+        }
+
+        else {
+          if (a == ""){
+            alert("Email must be filled out");
+            return false;
+          }
+
+          else {
+            if ((a.match(/@/g)).length != 1) {
+            alert("Email cannot have more than 1 '@'");
+            return false;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+function validateContact() {
+  let x = document.ContactForm.name.value;
+  let y = document.ContactForm.email.value;
+  let z = document.ContactForm.stream.value;
+  let a = document.ContactForm.message.value;
+  if (x == "") {
+    alert("Name cannot be empty");
+    return false;
+  }
+
+  else {
+    if (y == "") {
+      alert("Email cannot be empty");
+      return false;
+    }
+
+    else {
+      if (z == "Blank") {
+        alert("Stream must be selected");
+        return false;
+      }
+
+      else {
+        if (a == "") {
+          alert("Message cannot be empty");
+          return false;
+        }
+
+        else {
+          validateEmail(y);
+        }
+      }  
+    }
+  }
+}
